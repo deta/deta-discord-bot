fe138217e730fb8a15d2407c14ebab7c1d5d6957
Lemonyte <49930425+lemonyte@users.noreply.github.com>
2023-06-22 15:42:13 -0700

    Clean up github module

--- modules/github.py
+++ modules/github.py
@@ -1,11 +1,11 @@
 from enum import Enum
-from typing import List
 
 import httpx
-from discohook import Choice, Interaction, StringOption, command
+from discohook import Choice, Client, Embed, Interaction, StringOption, command
 from thefuzz import fuzz
 
-QUERY = """query searchResults($search_string: String!, $type: SearchType!, $num: Int!) {
+QUERY = (
+    """query searchResults($search_string: String!, $type: SearchType!, $num: Int!) {
     search(type: $type, query: $search_string, first: $num) {
         nodes {
             ... on Discussion {
@@ -25,53 +25,25 @@ QUERY = """query searchResults($search_string: String!, $type: SearchType!, $num
             }
         }
     }
-}""".replace("    ", "").replace("\n}", "}").lstrip()
+}""".replace(
+        "    ", ""
+    )
+    .replace("\n}", "}")
+    .lstrip()
+)
 
 
-class GithubResultType(Enum):
+class GitHubResultType(Enum):
+    """Maps the type option to the GitHub search filter."""
+
     ISSUE = "is:issue"
     PULL_REQUEST = "is:pr"
     DISCUSSION = ""  # Discussion ignores the 'is:' filter
 
 
-async def search_github(
-    search_string: str,
-    type: GithubResultType,  # The API doesn't distinguish between issues and PRs here.
-    num: int = 5,
-) -> List[str]:
-    if type == GithubResultType.ISSUE or type == GithubResultType.PULL_REQUEST:
-        type_string = "ISSUE"
-    elif type == GithubResultType.DISCUSSION:
-        type_string = "DISCUSSION"
-    async with httpx.AsyncClient() as client:
-        response = await client.post(
-            "https://api.github.com/graphql",
-            json={
-                "query": QUERY,
-                "variables": {
-                    "search_string": search_string,
-                    "type": type_string,
-                    "num": num,
-                },
-            },
-            headers={
-                # "Authorization": f"Bearer {GITHUB_TOKEN}",
-                "Content-Type": "application/json",
-            },
-        )
-    if response.status_code != 200:
-        return []
-    body = await response.json()
-    try:
-        results = body["data"]["search"]["nodes"]
-    except KeyError:
-        return []
-    return [f"[#{result['number']}]({result['url']}): {result['title']}" for result in results]
-
-
 @command(
     name="github",
-    description="Search github.",
+    description="Search GitHub.",
     options=[
         StringOption(
             name="topic",
@@ -87,25 +59,49 @@ async def search_github(
             name="type",
             description="Type of result to search for.",
             required=True,
-            choices=[Choice(type.name, type.name) for type in GithubResultType],
+            choices=[Choice(type.name, type.name) for type in GitHubResultType],
         ),
     ],
 )
-async def github_command_handler(interaction: Interaction, topic: str, repository: str, type: str):
-    type_ = GithubResultType(type)
-    search_string = f"repo:{repository} {topic or ''} {type_}"
-    results = await search_github(search_string, type_)
+async def github(interaction: Interaction, topic: str, repository: str, type: str):
+    result_type = GitHubResultType[type.upper()]
+    search_string = f"repo:{repository} {topic or ''} {result_type.value}"
+    async with httpx.AsyncClient() as client:
+        response = await client.post(
+            "https://api.github.com/graphql",
+            json={
+                "query": QUERY,
+                "variables": {
+                    "search_string": search_string,
+                    # The API doesn't distinguish between issues and PRs here.
+                    "type": result_type.name
+                    if result_type == GitHubResultType.DISCUSSION
+                    else GitHubResultType.ISSUE.name,
+                    "num": 5,
+                },
+            },
+            headers={
+                "Content-Type": "application/json",
+            },
+        )
+    results = []
+    if response.status_code == 200:
+        body = await response.json()
+        try:
+            results = [
+                f"[#{result['number']}]({result['url']}): {result['title']}"
+                for result in body["data"]["search"]["nodes"]
+            ]
+        except KeyError:
+            pass
     if results:
-        await interaction.response(embeds=[list_embed_builder(results)])
+        # TODO: configurable color
+        await interaction.response(embed=Embed(title="placeholder", description="\n".join(results), color=0xEE4196))
     else:
         await interaction.response("No results found.", ephemeral=True)
 
 
-def list_embed_builder(x):
-    raise NotImplementedError()
-
-
-@github_command_handler.autocomplete("repository")
+@github.autocomplete("repository")
 async def autocomplete_repository(interaction: Interaction, value: str):
     async with httpx.AsyncClient() as client:
         # TODO: implement cache
@@ -120,114 +116,5 @@ async def autocomplete_repository(interaction: Interaction, value: str):
     await interaction.autocomplete(choices)
 
 
-# async def search_discussions({
-#     name: 'discussion',
-#     description: 'Search for a discussion on github.',
-#     global: true,
-
-#     options: [
-#         {
-#             name= 'repository',
-#             description: 'The repository to search within',
-#             type= 'Integer',
-#             choices: [
-#                 {
-#                     name: 'SvelteKit',
-#                     value: Repos.SVELTE_KIT,
-#                 },
-#             ],
-#             required: true,
-#         },
-#         {
-#             name: 'topic',
-#             description: 'What to search for',
-#             type= 'String',
-#         },
-#     ],
-
-#     run: async ({ interaction }) =>
-#         await github_command_handler(interaction, GithubResultType.DISCUSSION),
-# })
-
-
-# async def search_issues({
-#     name: 'issue',
-#     description: 'Search for an issue on github',
-#     global: true,
-
-#     options: [
-#         {
-#             name: 'repository',
-#             description: 'The repository to search within',
-#             type= 'Integer',
-#             choices: [
-#                 {
-#                     name: 'Svelte',
-#                     value: Repos.SVELTE,
-#                 },
-#                 {
-#                     name: 'SvelteKit',
-#                     value: Repos.SVELTE_KIT,
-#                 },
-#                 {
-#                     name: 'Language Tools',
-#                     value: Repos.LANGUAGE_TOOLS,
-#                 },
-#             ],
-#             required: true,
-#         },
-#         {
-#             name: 'topic',
-#             description: 'What to search for',
-#             type= 'String',
-#         },
-#     ],
-
-#     run: async ({ interaction }) =>
-#         await github_command_handler(interaction, GithubResultType.ISSUE),
-# });
-
-
-# async def serach_pull_requests({
-#     name: 'pr',
-#     description: 'Search for a pull request on github',
-#     global: true,
-
-#     options: [
-#         {
-#             name: 'repository',
-#             description: 'The repository to search within',
-#             type= 'Integer',
-#             choices: [
-#                 {
-#                     name: 'Svelte',
-#                     value: Repos.SVELTE,
-#                 },
-#                 {
-#                     name: 'SvelteKit',
-#                     value: Repos.SVELTE_KIT,
-#                 },
-#                 {
-#                     name: 'RFCs',
-#                     value: Repos.RFCS,
-#                 },
-#                 {
-#                     name: 'Language Tools',
-#                     value: Repos.LANGUAGE_TOOLS,
-#                 },
-#             ],
-#             required: true,
-#         },
-#         {
-#             name: 'topic',
-#             description: 'What to search for',
-#             type= 'String',
-#         },
-#     ],
-
-#     run: async ({ interaction }) =>
-#         await github_command_handler(
-#             interaction,
-#             GithubResultType.PULL_REQUEST,
-#         ),
-# });
+def setup(client: Client):
+    client.add_commands(github)
