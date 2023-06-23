4ecccb9f18e05b3f705fedc7ea46ed5d0062ff2e
Lemonyte <49930425+lemonyte@users.noreply.github.com>
2023-06-22 18:48:17 -0700

    Cache repos, add auth, add typing, other fixes
    sorry, its a large and messy commit

--- modules/github.py
+++ modules/github.py
@@ -1,4 +1,6 @@
+import os
 from enum import Enum
+from typing import Iterable, NamedTuple
 
 import httpx
 from discohook import Choice, Client, Embed, Interaction, StringOption, command
@@ -12,16 +14,25 @@ QUERY = (
                 title
                 number
                 url
+                repository {
+                    name
+                }
             }
             ... on Issue {
                 title
                 number
                 url
+                repository {
+                    name
+                }
             }
             ... on PullRequest {
                 title
                 number
                 url
+                repository {
+                    name
+                }
             }
         }
     }
@@ -29,28 +40,41 @@ QUERY = (
         "    ", ""
     )
     .replace("\n}", "}")
-    .lstrip()
+    .strip()
 )
+
+GITHUB_TOKEN = os.getenv("GITHUB_TOKEN", "")
+repos_cache = []
 
 
 class GitHubResultType(Enum):
-    """Maps the type option to the GitHub search filter."""
+    """Maps a type option to the corresponding GitHub search filter."""
 
     ISSUE = "is:issue"
     PULL_REQUEST = "is:pr"
     DISCUSSION = ""  # Discussion ignores the 'is:' filter
 
 
+class GitHubSearchResult(NamedTuple):
+    """Represents an issue, pull request, or discussion as part of a GitHub search result."""
+
+    title: str
+    number: int
+    url: str
+    repository: str
+
+
+def format_search_results(results: Iterable[GitHubSearchResult]) -> list[str]:
+    formatted_results = []
+    for result in results:
+        formatted_results.append(f"[{result.repository}#{result.number}]({result.url}): {result.title}")
+    return formatted_results
+
+
 @command(
     name="github",
     description="Search GitHub.",
     options=[
-        StringOption(
-            name="repository",
-            description="The repository to search in.",
-            required=True,
-            autocomplete=True,
-        ),
         StringOption(
             name="type",
             description="Type of result to search for.",
@@ -61,11 +85,23 @@ class GitHubResultType(Enum):
             name="query",
             description="What to search for.",
         ),
+        StringOption(
+            name="repository",
+            description="The repository to search in.",
+            autocomplete=True,
+        ),
     ],
 )
-async def github(interaction: Interaction, query: str, repository: str, type: str):
+async def github(interaction: Interaction, type: str, query: str, repository: str):
+    if not GITHUB_TOKEN:
+        await interaction.response("GitHub token has not been provided.", ephemeral=True)
+        return
     result_type = GitHubResultType[type.upper()]
-    search_string = f"repo:{repository} {query or ''} {result_type.value}"
+    if repository:
+        search_string = f"repo:deta/{repository}"
+    else:
+        search_string = "org:deta"
+    search_string += f" {query or ''} {result_type.value}"
     async with httpx.AsyncClient() as client:
         response = await client.post(
             "https://api.github.com/graphql",
@@ -81,34 +117,42 @@ async def github(interaction: Interaction, query: str, repository: str, type: st
                 },
             },
             headers={
+                "Authorization": f"Bearer {GITHUB_TOKEN}",
                 "Content-Type": "application/json",
             },
         )
     results = []
     if response.status_code == 200:
-        body = await response.json()
         try:
-            results = [
-                f"[#{result['number']}]({result['url']}): {result['title']}"
-                for result in body["data"]["search"]["nodes"]
+            nodes = [
+                GitHubSearchResult(
+                    title=node["title"],
+                    number=node["number"],
+                    url=node["url"],
+                    repository=node["repository"]["name"],
+                )
+                for node in response.json()["data"]["search"]["nodes"]
             ]
+            results = format_search_results(nodes)
         except KeyError:
             pass
     if results:
         # TODO: configurable color
-        await interaction.response(embed=Embed(title="placeholder", description="\n".join(results), color=0xEE4196))
+        await interaction.response(embed=Embed(title="Search Results", description="\n".join(results), color=0xEE4196))
     else:
         await interaction.response("No results found.", ephemeral=True)
 
 
 @github.autocomplete("repository")
 async def autocomplete_repository(interaction: Interaction, value: str):
-    async with httpx.AsyncClient() as client:
-        # TODO: implement cache
-        print("FETCHING REPOS")
-        repos = await client.get("https://api.github.com/orgs/deta/repos?sort=updated")
+    if not repos_cache:
+        async with httpx.AsyncClient() as client:
+            print("FETCHING REPOS")
+            response = await client.get("https://api.github.com/orgs/deta/repos?sort=updated")
+            if response.status_code == 200:
+                repos_cache.extend(response.json())
     choices = []
-    for repo in repos.json():
+    for repo in repos_cache:
         name = repo["name"]
         ratio = fuzz.ratio(name, value.lower())
         if len(choices) < 25 < ratio:
